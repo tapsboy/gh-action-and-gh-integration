c6c75332adc1c92566ff8817218fcc93d8df929a
Tapan Shah <tapsboy@gmail.com>
2020-08-07 00:45:57 -0400

    try

--- cypress/integration/spec.js
+++ cypress/integration/spec.js
@@ -2,6 +2,6 @@ it('works', () => {
   expect(2 + 3).to.equal(5)
 })
 
-it('doesnt works', () => {
-  expect(2 + 3).to.equal(6)
-})
+// it('doesnt works', () => {
+//   expect(2 + 3).to.equal(6)
+// })
